02fad8aaeba36d039a1b9050c55f5c2f327110dc
athuljain <athuljainkj2@gmail.com>
2025-06-04 08:51:13 +0530

    zod error fixing

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,70 +1,124 @@
 
-import {z, ZodIssue} from 'zod'
+import {z} from 'zod'
 import { prisma } from './lib/prisma';
 import { redirect } from 'next/navigation';
 
 
-const recipeSchema=z.object({
-    title : z.string(),
-    description : z.string(),
-    ingredients:z.array(z.string()),
-    steps:z.array(z.string()),
-    image:z.string(),
-    category:z.string(),
-    cookingTimeInMinutes:z.number().int().positive(),
-    authorId: z.string()
-})
-
-
-export async function createRecipe(
-    ingredients:string[],
-    steps:string[],
-    authorId:string,
-    formData:FormData
-) {
-    let recipe;
-    try{
-
-        const payload={
-            title:formData.get("title"),
-            description:formData.get("description"),
-            image:formData.get("image"),
-            category:formData.get('category'),
-            cookingTimeInMinutes:Number(formData.get('cookingTimeInMinutes')),
-            ingredients,
-            steps,
-            authorId
-        }
-        const parsedData= recipeSchema.safeParse(payload);
-
-    
-        if (! parsedData.success) {
-            // If parsing failed, throw an error with details of the validation errors
-            throw new Error(
-                parsedData.error.errors
-                    .map((err: ZodIssue) => err.message)
-                    .join('\n')
-            );
-        }
-
-        recipe=await prisma.recipe.create({
-            data:parsedData.data
-        }) 
+const recipeSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  ingredients: z.array(z.string()).min(1, "At least one ingredient is required"),
+  steps: z.array(z.string()).min(1, "At least one step is required"),
+  image: z.string().min(1, "Image is required"),
+  category: z.string().min(1, "Category is required"),
+  cookingTimeInMinutes: z.coerce.number().int().positive("Cooking time must be a positive number"),
+  authorId: z.string().min(1, "Author ID is required"),
+});
 
 
 
-   }catch (error: unknown) {
-    if (error instanceof Error) {
-        console.log(error);
-        throw new Error(error.message || 'Internal server error');
-    } else {
-        console.log('Unknown error:', error);
-        throw new Error('Internal server error');
-    }
-}
-
-redirect(`recipes/${recipe.recipeId}`)
+// export async function createRecipe(
+//     ingredients:string[],
+//     steps:string[],
+//     authorId:string,
+//     formData:FormData
+// ) {
+//     let recipe;
+//     try{
 
 
+//      const payload = {
+//   title: String(formData.get("title") || ""),
+//   description: String(formData.get("description") || ""),
+//   image: String(formData.get("image") || ""),
+//   category: String(formData.get("category") || ""),
+//   cookingTimeInMinutes: Number(formData.get("cookingTimeInMinutes") || 0),
+//   ingredients: z.array(z.string()).min(1, "At least one ingredient is required"),
+// steps: z.array(z.string()).min(1, "At least one step is required"),
+//   authorId
+// };
+// console.log("🐛 Check payload before validation:", payload);
+
+// console.log("➡️ Raw form data values:");
+// console.log({
+//   title: formData.get("title"),
+//   description: formData.get("description"),
+//   image: formData.get("image"),
+//   category: formData.get("category"),
+//   cookingTimeInMinutes: formData.get("cookingTimeInMinutes"),
+//   ingredients,
+//   steps,
+//   authorId,
+// });
+
+
+
+
+
+//         const parsedData = recipeSchema.safeParse(payload);
+
+// if (!parsedData.success) {
+//   console.error("❌ Zod validation errors:", parsedData.error.errors);
+//   throw new Error(
+//     parsedData.error.errors
+//       .map((err: ZodIssue) => err.message)
+//       .join('\n')
+//   );
+// }
+
+// // ✅ Only log .data if safeParse was successful
+// console.log("✅ Parsed payload:", parsedData.data);
+
+// recipe = await prisma.recipe.create({
+//   data: parsedData.data,
+// });
+
+//         console.log("create Recipe:",recipe)
+
+
+//   } catch (error: unknown) {
+//   if (error instanceof Error) {
+//     console.error("Validation or Prisma error:", error.message);
+//     throw new Error(error.message || "Internal server error");
+//   }
+//   console.error("Unknown error:", error);
+//   throw new Error("Internal server error");
+// }
+
+
+// redirect(`recipes/${recipe.recipeId}`)
+
+
+// }
+
+
+
+export async function createRecipe(formData: FormData) {
+  const payload = {
+    title: String(formData.get("title") || ""),
+    description: String(formData.get("description") || ""),
+    image: String(formData.get("image") || ""),
+    category: String(formData.get("category") || ""),
+    cookingTimeInMinutes: Number(formData.get("cookingTimeInMinutes") || 0),
+    ingredients: formData.getAll("ingredients") as string[],
+    steps: formData.getAll("steps") as string[],
+    authorId: String(formData.get("authorId") || ""),
+  };
+
+  console.log("🐛 Check payload before validation:", payload);
+
+  const parsedData = recipeSchema.safeParse(payload);
+  if (!parsedData.success) {
+    console.error("❌ Zod validation errors:", parsedData.error.errors);
+    throw new Error(parsedData.error.errors.map((err) => err.message).join('\n'));
+  }
+
+  const recipe = await prisma.recipe.create({
+    data: parsedData.data,
+  });
+
+
+  
+  redirect(`recipes/${recipe.recipeId}`);
 }
 
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,93 +1,84 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
-import Link from "next/link"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
-const Header=()=>{
-    return(
-             <header className='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md py-4'>
-            <div className='container mx-auto flex justify-between items-center px-4'>
-              <Link href='/' className='text-3xl font-bold text-white'>
-          Foodies - The Recipe Book
+const Header = () => {
+  return (
+    <header className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <Link href="/" className="text-3xl font-bold text-white">
+          Dimples - The Recipe Book
         </Link>
-                <nav>
-                    <ul className='flex space-x-8 items-center'>
-                        <li>
-                           <Link href='/recipes/create-edit' className='items-center text-white hover:text-gray-300 transition-colors duration-300'>
-    New Recipe
-</Link>
-                        </li>
-                        <li className='relative' >
-                            <button
-                                className='text-white hover:text-gray-300 transition-colors duration-300'
-                              
-                            >
-                                Categories
-                            </button>
-                            <ul
-                                className={`absolute top-full left-0 bg-white shadow-lg py-2 rounded-md mt-1 `}
-                            >
-                              
-                                    <li  className='px-4 py-2'>
-                                        <Link href={'/'}
-                                          
-                                            className='text-gray-800 hover:text-blue-500 transition-colors duration-300'
-                                        >
-                                            category
-                                        </Link>
-                                    </li>
-                            
-                            </ul>
-                        </li>
-                       
-                            <li className='relative' >
-                                <button
-                                    className='text-white hover:text-gray-300 transition-colors duration-300'
-                                   
-                                >
-                                    Recipes
-                                </button>
-                                <ul
-                                    className={`absolute top-full left-0 bg-white shadow-lg py-2 rounded-md mt-1 `}
-                                >
-                                    <li className='px-4 py-2'>
-                                    <Link
-  href="/recipes/my/all"
-  className="text-gray-800 hover:text-blue-500 transition-colors duration-300"
->
-  Mine
-</Link>
+        <nav>
+          <ul className="flex space-x-8 items-center">
+            <li>
+              <Link
+                href="/recipes/create-edit"
+                className="items-center text-white hover:text-gray-300 transition-colors duration-300"
+              >
+                New Recipe
+              </Link>
+            </li>
+            <li className="relative">
+              <button className="text-white hover:text-gray-300 transition-colors duration-300">
+                Categories
+              </button>
+              <ul
+                className={`absolute top-full left-0 bg-white shadow-lg py-2 rounded-md mt-1 `}
+              >
+                <li className="px-4 py-2">
+                  <Link
+                    href={"/"}
+                    className="text-gray-800 hover:text-blue-500 transition-colors duration-300"
+                  >
+                    category
+                  </Link>
+                </li>
+              </ul>
+            </li>
 
-                                    </li>
-                                    <li className='px-4 py-2'>
-                                     <Link
-  href="/recipes/my/saved"
-  className="text-gray-800 hover:text-blue-500 transition-colors duration-300"
->
-  Saved
-</Link>
+            <li className="relative">
+              <button className="text-white hover:text-gray-300 transition-colors duration-300">
+                Recipes
+              </button>
+              <ul
+                className={`absolute top-full left-0 bg-white shadow-lg py-2 rounded-md mt-1 `}
+              >
+                <li className="px-4 py-2">
+                  <Link
+                    href="/recipes/my/all"
+                    className="text-gray-800 hover:text-blue-500 transition-colors duration-300"
+                  >
+                    Mine
+                  </Link>
+                </li>
+                <li className="px-4 py-2">
+                  <Link
+                    href="/recipes/my/saved"
+                    className="text-gray-800 hover:text-blue-500 transition-colors duration-300"
+                  >
+                    Saved
+                  </Link>
+                </li>
+              </ul>
+            </li>
 
-                                    </li>
-                                </ul>
-                            </li>
-                        
+            <li>
+              <SignedOut>
+                <SignInButton>
+                  <button className="items-center text-white hover:text-gray-300 transition-colors duration-300">
+                    Sign in
+                  </button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-                        <li>
-                            <SignedOut>
-                                <SignInButton>
-                                    <button className='items-center text-white hover:text-gray-300 transition-colors duration-300'>
-                                        Sign in
-                                    </button>
-                                </SignInButton>
-                            </SignedOut>
-                            <SignedIn>
-                                <UserButton />
-                            </SignedIn>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-
-    )
-}
-
-export default Header
+export default Header;
